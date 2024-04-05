bbabce292f9cf3c1bd30e0b9dc01bd84f5dbf5b8
Hannah Dadd <hannahdadd@hannahs-air.home>
2024-04-05 22:31:43 +0100

    send message when work sent

--- app/src/main/java/com/example/getitwrite/views/messages/SendWorkview.kt
+++ app/src/main/java/com/example/getitwrite/views/messages/SendWorkview.kt
@@ -1,8 +1,11 @@
 package com.example.getitwrite.views.messages
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -25,6 +28,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.getitwrite.Colours
+import com.example.getitwrite.modals.Message
 import com.example.getitwrite.modals.Proposal
 import com.example.getitwrite.modals.RequestCritique
 import com.example.getitwrite.modals.User
@@ -39,14 +43,16 @@ import java.util.UUID
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SendWorkView(user2Id: String, user: User, proposals: List<Proposal>, closeAction: () -> Unit) {
+fun SendWorkView(user2Id: String, user: User, proposals: List<Proposal>, chatID: String, closeAction: () -> Unit) {
     var errorString = remember { mutableStateOf("") }
     val title = remember { mutableStateOf("") }
     val text = remember { mutableStateOf("") }
     var proposal = remember { mutableStateOf<Proposal?>(null) }
     val sheetState = rememberModalBottomSheetState()
     var showBottomSheet by remember { mutableStateOf(false) }
-    Column(modifier = Modifier.padding(vertical = 10.dp)) {
+    Column(modifier = Modifier
+        .fillMaxHeight()
+        .padding(10.dp), verticalArrangement = Arrangement.spacedBy(10.dp)) {
         if (showBottomSheet) {
             ModalBottomSheet(
                 onDismissRequest = {
@@ -76,7 +82,12 @@ fun SendWorkView(user2Id: String, user: User, proposals: List<Proposal>, closeAc
                 }
             }
         )
-        QuestionSection(response = text, question = "Text:")
+        OutlinedTextField(value = text.value,
+            maxLines = 10,
+            onValueChange = { text.value = it },
+            modifier = Modifier.fillMaxWidth().height(150.dp),
+            label = { Text(text = "Text") }
+        )
         ErrorText(error = errorString)
         Button(
             modifier = Modifier.fillMaxWidth(),
@@ -87,6 +98,8 @@ fun SendWorkView(user2Id: String, user: User, proposals: List<Proposal>, closeAc
                     Firebase.firestore.collection("users").document(user2Id)
                         .collection("requestCritiques").document(id).set(requestCritique)
                         .addOnSuccessListener {
+                            val message = Message(content = "WORK SENT!\\n${user.displayName} sent their work entitled '${requestCritique.title}'", created = Timestamp.now(), senderID = user.id)
+                            Firebase.firestore.collection("chats").document(chatID).set(message)
                             closeAction()
                         }
                         .addOnFailureListener {
@@ -106,10 +119,27 @@ fun SendWorkView(user2Id: String, user: User, proposals: List<Proposal>, closeAc
 @Composable
 fun SelectProposalView(proposals: List<Proposal>, user: User, selectProposal: (Proposal) -> Unit) {
     val usersProposals = proposals.filter { it.writerId == user.id }
-    LazyColumn {
-        items(usersProposals) { proposal ->
-            ProposalView(proposal) {
-                selectProposal(it)
+
+    if (usersProposals.isEmpty()) {
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(10.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            Text(text = "You have no book proposals. Create one on the 'search' screen to swap work with other users.")
+        }
+    } else {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(10.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            items(usersProposals) { proposal ->
+                ProposalView(proposal) {
+                    selectProposal(it)
+                }
             }
         }
     }
--- app/src/main/java/com/example/getitwrite/views/proposals/ProposalsViewModel.kt
+++ app/src/main/java/com/example/getitwrite/views/proposals/ProposalsViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import com.example.getitwrite.modals.Proposal
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.firestore
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.tasks.await
@@ -11,7 +12,7 @@ import kotlinx.coroutines.tasks.await
 class ProposalsViewModel : ViewModel() {
     val proposalsFlow = flow {
         val documents = Firebase.firestore.collection("proposals")
-            .orderBy("timestamp")
+            .orderBy("timestamp", Query.Direction.DESCENDING)
             .get().await()
         val items = documents.map { doc ->
             Proposal(doc.id, doc.data)
