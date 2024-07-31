71580ae7d17a5ab643fd8f7b77b8f90fd04d2a11
Hannah Dadd <hannahdadd@hannahs-air.home>
2024-07-31 07:50:19 +0100

    ai promo

--- app/src/main/java/hannah/bd/getitwrite/views/feed/HomeFeed.kt
+++ app/src/main/java/hannah/bd/getitwrite/views/feed/HomeFeed.kt
@@ -1,12 +1,23 @@
 package hannah.bd.getitwrite.views.feed
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Email
@@ -15,8 +26,17 @@ import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import hannah.bd.getitwrite.R
 import hannah.bd.getitwrite.views.components.SquareTileButton
 
@@ -39,27 +59,28 @@ fun HomeFeed() {
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(8.dp)) {
         item {
-            Text(
-                text = "Welcome",
-                style = MaterialTheme.typography.displayMedium,
-                color = MaterialTheme.colorScheme.onSurface,
-                modifier = Modifier.padding(16.dp)
-            )
-        }
-        item {
-            LazyRow(
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                modifier = Modifier.padding(16.dp)
+            Column(
+                modifier = Modifier.padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
             ) {
-                item {
-                    SquareTileButton(
-                        title = "To critique",
-                        wordCount = "100 words",
-                        backgroundColour = MaterialTheme.colorScheme.primary,
-                        textColour = MaterialTheme.colorScheme.onPrimary,
-                        icon = Icons.Default.Notifications,
-                        onClick = {}
-                    )
+                Text(
+                    text = "Welcome",
+                    style = MaterialTheme.typography.displayMedium,
+                    color = MaterialTheme.colorScheme.onSurface,
+                )
+                LazyRow(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                ) {
+                    item {
+                        SquareTileButton(
+                            title = "To critique",
+                            wordCount = "100 words",
+                            backgroundColour = MaterialTheme.colorScheme.primary,
+                            textColour = MaterialTheme.colorScheme.onPrimary,
+                            icon = Icons.Default.Notifications,
+                            onClick = {}
+                        )
+                    }
                 }
             }
         }
@@ -97,6 +118,33 @@ fun HomeFeed() {
                         )
                     }
                 }
+            }
+        }
+        item {
+            Box(
+                modifier = Modifier
+                    .height(250.dp)
+                    .fillMaxWidth()
+                    .clickable(onClick = {  }),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.aibg),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .padding(10.dp)
+                        .clip(RoundedCornerShape(10.dp))
+                )
+                Text(
+                    text = "Get AI feedback on your writing, instantly",
+                    color = Color.Black,
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.background(Color.White).align(Alignment.Center).width(200.dp)
+                )
             }
         }
         item {
