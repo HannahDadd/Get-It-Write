a154c18a7d53b4ae4c167fb3d97c00d2a66ba2ae
Hannah Dadd <hannahdadd@hannahs-air.home>
2024-08-04 17:59:57 +0100

    get rand

--- app/src/main/java/hannah/bd/getitwrite/views/positivityCorner/PositivityPopUp.kt
+++ app/src/main/java/hannah/bd/getitwrite/views/positivityCorner/PositivityPopUp.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -40,6 +41,7 @@ import hannah.bd.getitwrite.theme.AppTypography
 import hannah.bd.getitwrite.views.components.CheckInput
 import hannah.bd.getitwrite.views.components.ErrorText
 import hannah.bd.getitwrite.views.components.ReportAndBlockUser
+import hannah.bd.getitwrite.views.feed.Screen
 import hannah.bd.getitwrite.views.proposals.ProposalsViewModel
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.tasks.await
@@ -49,111 +51,160 @@ import java.util.UUID
 fun PositivityPopUp(user: User,
     close: () -> Unit
 ) {
-    val peice by PositivityViewModel().randPiece.collectAsState(initial = null)
+    var piece = remember { mutableStateOf<RequestPositivity?>(null) }
     val comment = remember { mutableStateOf("") }
     var errorString = remember { mutableStateOf<String?>(null) }
+    var isLoading by remember { mutableStateOf(true) }
 
-//    LaunchedEffect(Unit) {
-//        getIds(
-//            onSuccess = { returnedIds ->
-//                ids = returnedIds
-//                isLoading = false
-//            },
-//            onError = { exception ->
-//                errorString.value = exception.message
-//                isLoading = false
-//            }
-//        )
-//    }
+    LaunchedEffect(Unit) {
+        getRandPeice(
+            onSuccess = {
+                piece.value = it
+                isLoading = false
+            },
+            onError = { exception ->
+                errorString.value = exception.message
+                isLoading = false
+            }
+        )
+    }
 
-    LazyColumn(
-        modifier = Modifier.padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(10.dp)
-    ) {
-        peice?.let {
-            item {
-                Text(text = it.text)
-            }
-            item {
-                ReportAndBlockUser(
-                    userToBlock = it.writerId,
-                    user = user,
-                    contentToReport = it,
-                    contentToReportType = ContentToReportType.REQUESTCRITIQUE,
-                    questionId = null,
-                    chatId = null
-                )
-            }
-            item {
-                OutlinedTextField(value = comment.value,
-                    maxLines = 5,
-                    onValueChange = { comment.value = it },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(150.dp),
-                    label = { Text(text = "Positive Comment") }
-                )
-            }
-            item {
-                ErrorText(error = errorString)
-            }
-            item {
-                Button(
-                    modifier = Modifier.fillMaxWidth(),
-                    onClick = {
-                        if(CheckInput.isStringGood(comment.value, 50)) {
-                            val id = it.id
-                            val newComments = it.comments.plus(Pair(user.displayName, comment.value))
-                            val critique = RequestPositivity(id = id, text = it.text, writerId = it.writerId, writerName = it.writerName, comments = newComments)
-                            Firebase.firestore.collection("users").document(it.writerId)
-                                .collection("positivityPeices").document(id).set(critique)
-                                .addOnSuccessListener {
-                                    Firebase.firestore.collection("positivityPeices").document(id)
-                                        .set(critique)
+    if (isLoading) {
+        Text("Loading...", modifier = Modifier.padding(16.dp))
+    } else {
+        errorString.value?.let {
+            ErrorText(error = it)
+        }?: run {
+            LazyColumn(
+                modifier = Modifier.padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                piece.value?.let {
+                    item {
+                        Text(text = it.text)
+                    }
+                    item {
+                        ReportAndBlockUser(
+                            userToBlock = it.writerId,
+                            user = user,
+                            contentToReport = it,
+                            contentToReportType = ContentToReportType.REQUESTCRITIQUE,
+                            questionId = null,
+                            chatId = null
+                        )
+                    }
+                    item {
+                        OutlinedTextField(value = comment.value,
+                            maxLines = 5,
+                            onValueChange = { comment.value = it },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(150.dp),
+                            label = { Text(text = "Positive Comment") }
+                        )
+                    }
+                    item {
+                        ErrorText(error = errorString)
+                    }
+                    item {
+                        Button(
+                            modifier = Modifier.fillMaxWidth(),
+                            onClick = {
+                                if (CheckInput.isStringGood(comment.value, 50)) {
+                                    val id = it.id
+                                    val newComments =
+                                        it.comments.plus(Pair(user.displayName, comment.value))
+                                    val critique = RequestPositivity(
+                                        id = id,
+                                        text = it.text,
+                                        writerId = it.writerId,
+                                        writerName = it.writerName,
+                                        comments = newComments
+                                    )
+                                    Firebase.firestore.collection("users").document(it.writerId)
+                                        .collection("positivityPeices").document(id).set(critique)
                                         .addOnSuccessListener {
-                                            close()
+                                            Firebase.firestore.collection("positivityPeices")
+                                                .document(id)
+                                                .set(critique)
+                                                .addOnSuccessListener {
+                                                    close()
+                                                }
+                                                .addOnFailureListener {
+                                                    errorString.value = it.message.toString()
+                                                }
                                         }
                                         .addOnFailureListener {
                                             errorString.value = it.message.toString()
                                         }
+                                } else {
+                                    errorString.value = CheckInput.errorStringText
                                 }
-                                .addOnFailureListener {
-                                    errorString.value = it.message.toString()
-                                }
-                        } else {
-                            errorString.value = CheckInput.errorStringText
+                            },
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = MaterialTheme.colorScheme.primary,
+                                contentColor = MaterialTheme.colorScheme.onPrimary
+                            )
+                        ) {
+                            Text(
+                                "Submit Critique",
+                                Modifier.padding(10.dp),
+                                style = AppTypography.titleMedium
+                            )
                         }
-                    },
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = MaterialTheme.colorScheme.primary,
-                        contentColor = MaterialTheme.colorScheme.onPrimary
-                    )
-                ) {
-                    Text("Submit Critique", Modifier.padding(10.dp), style = AppTypography.titleMedium)
-                }
+                    }
+                    item {
+                        it.comments.forEach {
+                            Divider()
+                            Text(it.key, Modifier.padding(10.dp), style = AppTypography.titleMedium)
+                            Text(
+                                it.value,
+                                Modifier.padding(10.dp),
+                                style = AppTypography.bodyMedium
+                            )
+                        }
+                    }
+                } ?: item { Text("Loading...") }
             }
-            item {
-                it.comments.forEach {
-                    Divider()
-                    Text(it.key, Modifier.padding(10.dp), style = AppTypography.titleMedium)
-                    Text(it.value, Modifier.padding(10.dp), style = AppTypography.bodyMedium)
-                }
-            }
-        } ?: item { Text("Loading...") }
+        }
     }
 }
 
-class PositivityViewModel : ViewModel() {
-    val randPiece = flow {
-        val ids = Firebase.firestore.collection("positivityPeices")
-            .document("ids").get().await()
-        val idsArray = ids["ids"] as MutableList<String>
-        val doc = Firebase.firestore.collection("positivityPeices")
-            .document(idsArray.random()).get().await()
-        doc.data?.let {
-            emit(RequestPositivity(id = doc.id, data = it))
-        } ?: run {
-            emit(null)
+fun getRandPeice(
+    onSuccess: (RequestPositivity) -> Unit,
+    onError: (Exception) -> Unit) {
+    Firebase.firestore.collection("positivityPeices")
+        .document("ids").get()
+        .addOnSuccessListener { document ->
+            if (document != null && document.exists()) {
+                val idsArray = document.data?.get("ids") as MutableList<String>
+                if (idsArray != null) {
+                    Firebase.firestore.collection("positivityPeices")
+                        .document(idsArray.random()).get()
+                    .addOnSuccessListener { document ->
+                        if (document != null && document.exists()) {
+                            val piece =
+                                document.data?.let { RequestPositivity(id = document.id, data = it) }
+                            if (piece != null) {
+                                onSuccess(piece)
+                            } else {
+                                onError(Exception("Positivity Peice is null"))
+                            }
+                        } else {
+                            onError(Exception("Data not found"))
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        onError(exception)
+                    }
+                } else {
+                    onError(Exception("Date in wrong format"))
+                }
+            } else {
+                onError(Exception("Data not found"))
+            }
         }
-    }
+        .addOnFailureListener { exception ->
+            onError(exception)
+        }
 }
