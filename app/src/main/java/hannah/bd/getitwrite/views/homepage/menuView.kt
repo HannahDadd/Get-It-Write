ec5484c8bbda89746dddc954f432f6c4a9de1e9b
Hannah Dadd <hannahbdadd@googlemail.com>
2026-09-26 13:51:11 +0100

    new icons

--- app/src/main/java/hannah/bd/getitwrite/views/homepage/menuView.kt
+++ app/src/main/java/hannah/bd/getitwrite/views/homepage/menuView.kt
@@ -10,12 +10,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -25,18 +21,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import hannah.bd.getitwrite.views.ghosts.ghostView
 import hannah.bd.getitwrite.R
 import hannah.bd.getitwrite.modals.AppDatabase
 import hannah.bd.getitwrite.modals.Stat
 import hannah.bd.getitwrite.modals.UserPreferences
-import hannah.bd.getitwrite.views.onboarding.OnboardingText
+import hannah.bd.getitwrite.views.ghosts.ghostView
 import hannah.bd.getitwrite.views.settings.settingsView
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -124,7 +116,7 @@ fun MenuView(
             Row {
 
                 Image(
-                    painter = painterResource(R.drawable.write_btn),
+                    painter = painterResource(R.drawable.menu_btn),
                     contentDescription = "Menu",
                     modifier = Modifier
                         .size(buttonSize)
